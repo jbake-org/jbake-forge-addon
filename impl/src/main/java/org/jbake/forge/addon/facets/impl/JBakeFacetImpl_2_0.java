763795aee8cb5f3b22ef210c0aef95cc10063c67
Manasa Mylavarapu <manimanasamylavarapu@gmail.com>
2015-05-15 18:32:52 +0530

    Modified file name parsing for page and post usinf JbakeUtil.Slug

--- impl/src/main/java/org/jbake/forge/addon/facets/impl/JBakeFacetImpl_2_0.java
+++ impl/src/main/java/org/jbake/forge/addon/facets/impl/JBakeFacetImpl_2_0.java
@@ -21,6 +21,7 @@ import org.jbake.forge.addon.types.ContentType;
 import org.jbake.forge.addon.types.PublishType;
 import org.jbake.forge.addon.types.TemplateType;
 import org.jbake.forge.addon.utils.ContentUtil;
+import org.jbake.forge.addon.utils.JBakeUtil;
 import org.jbake.forge.addon.utils.TemplateUtil;
 import org.jboss.forge.addon.dependencies.Coordinate;
 import org.jboss.forge.addon.dependencies.Dependency;
@@ -269,13 +270,13 @@ public class JBakeFacetImpl_2_0 extends AbstractJBakeFacet {
     public boolean createPage() throws IOException {
         Boolean isCreated = false;
         if (contentType == (ContentType.AsciiDoc)) {
-            ContentUtil.createFile(targetDirectory, contentType, pageTitle);
+            ContentUtil.createFile(targetDirectory, contentType, JBakeUtil.toSlug(pageTitle));
             isCreated = ContentUtil.writeFile(contentType, pageTitle, creationOrModificationDate, "page", tags, pageStatusType);
         } else if (contentType == (ContentType.HTML)) {
-            ContentUtil.createFile(targetDirectory, contentType, pageTitle);
+            ContentUtil.createFile(targetDirectory, contentType, JBakeUtil.toSlug(pageTitle));
             isCreated = ContentUtil.writeFile(contentType, pageTitle, creationOrModificationDate, "page", tags, pageStatusType);
         } else if (contentType == (ContentType.Markdown)) {
-            ContentUtil.createFile(targetDirectory, contentType, pageTitle);
+            ContentUtil.createFile(targetDirectory, contentType, JBakeUtil.toSlug(pageTitle));
             isCreated = ContentUtil.writeFile(contentType, pageTitle, creationOrModificationDate, "page", tags, pageStatusType);
         }
         return isCreated;
@@ -285,13 +286,13 @@ public class JBakeFacetImpl_2_0 extends AbstractJBakeFacet {
     public boolean createPost() throws IOException {
         Boolean isCreated = false;
         if (contentType == (ContentType.AsciiDoc)) {
-            ContentUtil.createFile(targetDirectory, contentType, postTitle);
+            ContentUtil.createFile(targetDirectory, contentType, JBakeUtil.toSlug(postTitle));
             isCreated = ContentUtil.writeFile(contentType, postTitle, creationOrModificationDate, "post", tags, postStatusType);
         } else if (contentType == (ContentType.HTML)) {
-            ContentUtil.createFile(targetDirectory, contentType, postTitle);
+            ContentUtil.createFile(targetDirectory, contentType, JBakeUtil.toSlug(postTitle));
             isCreated = ContentUtil.writeFile(contentType, postTitle, creationOrModificationDate, "post", tags, postStatusType);
         } else if (contentType == (ContentType.Markdown)) {
-            ContentUtil.createFile(targetDirectory, contentType, postTitle);
+            ContentUtil.createFile(targetDirectory, contentType, JBakeUtil.toSlug(postTitle));
             isCreated = ContentUtil.writeFile(contentType, postTitle, creationOrModificationDate, "post", tags, postStatusType);
         }
         return isCreated;
@@ -326,14 +327,14 @@ public class JBakeFacetImpl_2_0 extends AbstractJBakeFacet {
         String filePathStringForGradle = null;
         try {
             filePathStringForMaven = codeFolder.getCanonicalPath() + "/src/main/jbake";
-            filePathStringForGradle = codeFolder.getCanonicalPath() + "/src/jbake";
+          //  filePathStringForGradle = codeFolder.getCanonicalPath() + "/src/jbake";
         } catch (IOException e) {
             e.printStackTrace();
         }
 
         File f1 = new File(filePathStringForMaven);
-        File f2 = new File(filePathStringForGradle);
-        if (f1.exists() || f2.exists()) {
+      //  File f2 = new File(filePathStringForGradle);
+        if (f1.exists()) {
             return true;
         } else {
             return false;
